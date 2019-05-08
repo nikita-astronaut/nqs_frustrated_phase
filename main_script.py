7e91e6973ff89fb6ee8148b4a1926a91262dc0c3
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2019-05-08 14:31:28 +0300

    works!

--- main_script.py
+++ main_script.py
@@ -1,8 +1,9 @@
 import os
+import time
 import sys
 import numpy as np
 import torch
-
+import importlib
 
 def import_network(filename: str):
     module_name, extension = os.path.splitext(os.path.basename(filename))
@@ -21,18 +22,24 @@ def import_network(filename: str):
 
 
 def load_ground_state(number_spins, name):
-    f = open(name, 'rb')
+	f = open(name, 'r')
 
-    all_spins = np.zeros((0, number_spins))
-    signs = []
+	all_spins = np.zeros((0, number_spins))
+	signs = []
 
-    for line in f:
-    	spin, re, im = line.split('\t')
-    	spin = np.array(list(spin)) * 2 - 1
-    	all_spins = np.concatenate([all_spins, spin], axis = 0)
-    	signs.append(np.sign(float(re)))
+	for line in f:
+		spin, re, im = line.split('\t')
+		spin_array = []
+		for s in spin:
+			if s == '0':
+				spin_array.append(-1)
+			else:
+				spin_array.append(+1)
+		spin = np.array(spin_array)[np.newaxis, :]
+		all_spins = np.concatenate([all_spins, spin], axis = 0)
+		signs.append(np.sign(float(re)) * 0.5 + 0.5)
 
-    return torch.from_numpy(all_spins), (torch.from_numpy(signs)).type(torch.LongTensor)
+	return torch.from_numpy(all_spins).type(torch.FloatTensor), (torch.from_numpy(np.array(signs))).type(torch.LongTensor)
 
 
 def train_phase(ψ: torch.nn.Module, samples_train, target_train, config, samples_val, target_val, GPU = False):
@@ -59,8 +66,8 @@ def train_phase(ψ: torch.nn.Module, samples_train, target_train, config, sample
             _, predicted = torch.max(ψ(samples_val), dim=1)
             return float(torch.sum(target_val == predicted)) / target_val.size(0)
     val_losses = []
-    logging.info("Initial accuracy train : {:.2f}%".format(100 * accuracy(True)))
-    logging.info("Initial accuracy val : {:.2f}%".format(100 * accuracy(False)))
+    print("Initial accuracy train : {:.2f}%".format(100 * accuracy(True)))
+    print("Initial accuracy val : {:.2f}%".format(100 * accuracy(False)))
 
 
     idxs = np.arange(len(samples_train))
@@ -85,11 +92,11 @@ def train_phase(ψ: torch.nn.Module, samples_train, target_train, config, sample
     th = 100 * accuracy(True)
     vh = 100 * accuracy(False)
 
-    logging.info("Final accuracy train : {:.2f}%".format(th))
-    logging.info("Final accuracy val : {:.2f}%".format(vh))
+    print("Final accuracy train : {:.2f}%".format(th))
+    print("Final accuracy val : {:.2f}%".format(vh))
 
     finish = time.time()
-    logging.info("Done in {:.2f} seconds!".format(finish - start))
+    print("Done in {:.2f} seconds!".format(finish - start))
     if GPU:
         ψ = ψ.cpu()
     return ψ, [th], [vh]
@@ -99,28 +106,26 @@ def train_phase(ψ: torch.nn.Module, samples_train, target_train, config, sample
 
 #################### parameters setting here ###########################
 n_trials = 10 # how many times we run learning process from the beginning
-parameters_set = [0.0] # set possible values of J2 or any other tunable frustration parameter
+parameters_set = ['0.00', '0.55'] # set possible values of J2 or any other tunable frustration parameter
 exact_ground_states_signs = []
 
 log_file = open('./logs/J1J2_log.dat', 'w') # specify logfile 
 
 # other options
-number_spins = 25
+number_spins = 16
 magnetization = number_spins % 2
-samples = all_spins(number_spins, magnetization)
-
 
 for parameter in parameters_set:
-	exact_gs_vector_name = "./vectors_J1J2/j1j2_" + str(parameter) + '.vector' # specify how the vector name is obtained
-	exact_ground_states_signs.append(load_ground_state(samples, exact_gs_vector_name))
+	exact_gs_vector_name = "./4x4.exact/vector_" + str(parameter) + '.txt' # specify how the vector name is obtained
+	exact_ground_states_signs.append(load_ground_state(number_spins, exact_gs_vector_name))
 
 
-PhaseNet = import_network("../models/J1J2_model.py") # explicit path to model.py file
+PhaseNet = import_network("./models/J1J2_model.py") # explicit path to model.py file
 optimiser = lambda p: torch.optim.Adam(p.parameters(), lr=0.003)
 epochs = 100
 batch_size = 512
 loss = torch.nn.CrossEntropyLoss()
-train_ratio = 0.01 # train on 1 % of the total number of spin configurations
+train_ratio = 0.5 # train on 1 % of the total number of spin configurations
 
 ################### parameters setting ended #########################
 
@@ -134,7 +139,7 @@ for parameter, gs in zip(parameters_set, exact_ground_states_signs):
     samples = gs[0]
     phase_signs = gs[1]
 
-	log_file.write(str(parameter) + ' : ')
+    log_file.write(str(parameter) + ' : ')
     for trial_number in range(n_trials):
         ψ_phase = PhaseNet(number_spins)
 
@@ -142,7 +147,8 @@ for parameter, gs in zip(parameters_set, exact_ground_states_signs):
         np.random.shuffle(idxs)
         train_idxs = idxs[0:int(train_ratio * len(samples))]
         val_idxs = idxs[int(train_ratio * len(samples)):]
-        
+        train_history = []
+        val_history = [] 
         for i in range(epochs):
             ψ_phase, th, vh = train_phase(ψ_phase, samples[train_idxs], phase_signs[train_idxs], 
                                           options_dict, samples[val_idxs], phase_signs[val_idxs], GPU = True)
@@ -151,5 +157,5 @@ for parameter, gs in zip(parameters_set, exact_ground_states_signs):
         log_file.write(str(train_history[-1]) + '/')
         log_file.write(str(val_history[-1]) + ' ')
         log_file.flush()
-        print("For parameter = " + str(parameters) + ", iteration " + str(i) + " train accuracy: " + str(th[0]) + ", val accuracy " + str(vh[0]))
-
+        print("For parameter = " + str(parameter) + ", iteration " + str(i) + " train accuracy: " + str(th[0]) + ", val accuracy " + str(vh[0]))
+    log_file.write('\n')
