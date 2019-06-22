925a813f4f101cd6db67f94e1376ea415b6a98e1
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2019-06-22 23:18:36 +0300

    added plotting of e_tr dependence

--- plot_results.py
+++ plot_results.py
@@ -13,27 +13,43 @@ plt.rc('text', usetex = True)
 plt.rc('font', family = 'TeX Gyre Adventor', size = 14)
 plt.rc("pdf", fonttype=42)
 
-tr = float(sys.argv[2])    
-df_selected = df[df['train_ratio'] == tr]
 
-plt.errorbar(df_selected.j2.values + 0.005, df_selected.test_acc.values, df_selected.dtest_acc.values, fmt = 'o', markersize = 5,
-                         mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
-                         label = 'validation accuracy', color = 'red')
+if sys.argv[2] == 'train_ratio':
+    for j2 in np.unique(df['j2'].values):
+        df_selected = df[df['j2'] == j2]
 
-plt.errorbar(df_selected.j2.values, df_selected.train_acc.values, df_selected.dtrain_acc.values, fmt = 'd', markersize = 5,
-                         mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
-                         label = 'train accuracy', color = 'blue')
+        plt.errorbar(df_selected.train_ratio.values, df_selected.rest_acc.values, df_selected.drest_acc.values, fmt = 'o', markersize = 5,
+                                 mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
+                                 label = 'validation accuracy, $j_2 = ' + str(j2) + '$', color = 'red')
 
-plt.errorbar(df_selected.j2.values, df_selected.overlap.values, df_selected.doverlap.values, fmt = '*', markersize = 5,
-                         mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
-                         label = 'overlap', color = 'black')
+        plt.errorbar(df_selected.train_ratio.values, df_selected.train_acc.values, df_selected.dtrain_acc.values, fmt = 'd', markersize = 5,
+                                 mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
+                                 label = 'train accuracy, $j_2 = ' + str(j2) + '$', color = 'blue')
 
-plt.plot(df_selected.j2.values, df_selected.total_acc.values, markersize = 5,
-                         mec='black', mew=0.5, ls = '--',
-                         label = 'total accuracy', color = 'black')
-plt.xlabel('$J2 / J1$')
+        plt.errorbar(df_selected.train_ratio.values, df_selected.overlap.values, df_selected.doverlap.values, fmt = '*', markersize = 5,
+                                 mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
+                                 label = 'overlap, $j_2 = ' + str(j2) + '$', color = 'black')
+    plt.xlabel('$e_{\\mbox{tr}}$')
+    plt.xscale('log')
+else:
+    tr = float(sys.argv[2])
+    df_selected = df[df['train_ratio'] == tr]
+
+    plt.errorbar(df_selected.j2.values + 0.005, df_selected.test_acc.values, df_selected.dtest_acc.values, fmt = 'o', markersize = 5,
+                             mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
+                             label = 'validation accuracy', color = 'red')
+
+    plt.errorbar(df_selected.j2.values, df_selected.train_acc.values, df_selected.dtrain_acc.values, fmt = 'd', markersize = 5,
+                             mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
+                             label = 'train accuracy', color = 'blue')
+
+    plt.errorbar(df_selected.j2.values, df_selected.overlap.values, df_selected.doverlap.values, fmt = '*', markersize = 5,
+                             mec='black', mew=0.5, elinewidth=1.0, capsize=3.0, ecolor='black',
+                             label = 'overlap', color = 'black')
+    plt.xlabel('$j_2 / j_1$')
+    
 plt.ylabel('accuracy, \\%, overlap')
-plt.legend(loc = 'lower left', ncol = 1, numpoints = 1, fontsize = 12)
+plt.legend(loc = 'lower left', ncol = 2, numpoints = 1, fontsize = 8)
 plt.axhline(y=0.0, color='black', linestyle='--')
 plt.axhline(y=0.5, color='black', linestyle='--')
 plt.grid(True)
