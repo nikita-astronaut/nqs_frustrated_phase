8872f21ea6f9d30c21646fb172b82893fe539153
Tom Westerhout <t.westerhout@student.ru.nl>
2019-07-16 06:26:18 +0200

    3-layer dense network

---
+++ models/dense_32_ReLU_32_ReLU_32_ReLU_2.py
@@ -0,0 +1,12 @@
+import torch
+
+Net = lambda n: torch.nn.Sequential(
+    torch.nn.Linear(n, 32),
+    torch.nn.ReLU(),
+    torch.nn.Linear(32, 32),
+    torch.nn.ReLU(),
+    torch.nn.Linear(32, 32),
+    torch.nn.ReLU(),
+    torch.nn.Linear(32, 2, bias=False),
+)
+
